49edb46d3e7b3916440beb5b1fb2f9e91041da4c
John Ericson <John.Ericson@Obsidian.Systems>
2019-10-01 12:59:49 -0400

    Delete Nix
    I believe reflex-platform just uses `callCabal2nix` now; other Nix users
    can do the same.

--- default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {}, haskellPackages ? pkgs.haskellPackages }:
-
-{
-  jsaddle = haskellPackages.callPackage ./jsaddle {};
-  jsaddle-warp = haskellPackages.callPackage ./jsaddle-warp {};
-  jsaddle-wkwebview = haskellPackages.callPackage ./jsaddle-wkwebview {};
-  jsaddle-webkit2gtk = haskellPackages.callPackage ./jsaddle-webkit2gtk {};
-  jsaddle-clib = haskellPackages.callPackage ./jsaddle-clib {};
-}
