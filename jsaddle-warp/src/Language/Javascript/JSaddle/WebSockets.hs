f00e9ef0371cafc2064b0314feb861bfdc319d88
Matthew Bauer <mjbauer95@gmail.com>
2019-11-08 17:26:20 -0500

    Use triple equality
    This is a little safer for javascript equality.

--- jsaddle-warp/src/Language/Javascript/JSaddle/WebSockets.hs
+++ jsaddle-warp/src/Language/Javascript/JSaddle/WebSockets.hs
@@ -166,7 +166,7 @@ jsaddleJs = jsaddleJs' Nothing
 -- sed -e 's|\\|\\\\|g' -e 's|^|    \\|' -e 's|$|\\n\\|' -e 's|"|\\"|g' data/jsaddle.js | pbcopy
 jsaddleJs' :: Maybe ByteString -> Bool -> ByteString
 jsaddleJs' jsaddleUri refreshOnLoad = jsaddleCoreJs <> "\
-    \if(typeof global !== \"undefined\" && typeof require == \"function\") {\n\
+    \if(typeof global !== \"undefined\" && typeof require === \"function\") {\n\
     \    global.window = global;\n\
     \    global.WebSocket = require('ws');\n\
     \}\n\
