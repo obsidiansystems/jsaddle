7cdd1d44c0f0c8f002af94403fe8621f8e2576a9
Divam <dfordivam@gmail.com>
2020-05-04 20:05:25 +0900

    Remove generalBracket to fix build.

--- jsaddle/src/Language/Javascript/JSaddle/Types.hs
+++ jsaddle/src/Language/Javascript/JSaddle/Types.hs
@@ -789,11 +789,12 @@ instance MonadMask JSM where
       where q :: (ReaderT JSContextRef IO a -> ReaderT JSContextRef IO a) -> JSM a -> JSM a
             q unmask (JSM b) = JSM $ unmask b
 #if MIN_VERSION_exceptions(0,9,0)
-  generalBracket acquire release use =
-    JSM $ generalBracket
-      (unJSM acquire)
-      (\resource exitCase -> unJSM $ release resource exitCase)
-      (unJSM . syncAfter . use)
+  -- TODO: Fix syncAfter and enable this
+  -- generalBracket acquire release use =
+  --   JSM $ generalBracket
+  --     (unJSM acquire)
+  --     (\resource exitCase -> unJSM $ release resource exitCase)
+  --     (unJSM . syncAfter . use)
 #endif
 
 withLog :: String -> IO a -> IO a
