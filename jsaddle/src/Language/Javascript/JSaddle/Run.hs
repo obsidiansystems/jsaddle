380740020475d34f81638a695ea27b2c22dc9de7
Divam <dfordivam@gmail.com>
2020-09-02 15:18:52 +0900

    Stop the higher frame tries on exception

--- jsaddle/src/Language/Javascript/JSaddle/Run.hs
+++ jsaddle/src/Language/Javascript/JSaddle/Run.hs
@@ -62,6 +62,7 @@ import Data.Maybe
 import qualified Data.Map as M
 import Data.IORef (newIORef)
 import qualified Data.Text as T
+import GHCJS.Prim.Internal (primToJSVal)
 
 import Language.Javascript.JSaddle.Types
 --TODO: Handle JS exceptions
@@ -187,7 +188,20 @@ runJavaScriptInt sendReqsTimeout pendingReqsLimit sendReqsBatch = do
         GT -> do
           !syncBlockReq <- getSyncBlockReqForResult myDepth myRetVal
           let !newReadyFrames = M.insertWith (error "should be impossible: trying to return from a sync frame that has already returned") myDepth syncBlockReq oldReadyFrames
-              !newFrameTries = M.delete myDepth oldFrameTries
+          !newFrameTries <- case myRetVal of
+            Right _ -> pure (M.delete myDepth oldFrameTries)
+            Left _ -> do
+              let
+                (!newFrameTries, toStop) = M.split myDepth oldFrameTries
+                stopTry tryId = do
+                  mTryMVar <- atomically $ do
+                    currentTries <- readTVar tries
+                    writeTVar tries $! M.delete tryId currentTries
+                    return $ M.lookup tryId currentTries
+                  forM_ mTryMVar $ \v ->
+                    putMVar v $ Left $ primToJSVal $ PrimVal_String "Parent Try received an exception."
+              mapM_ stopTry (M.elems toStop)
+              pure newFrameTries
           return (oldDepth, newReadyFrames, newFrameTries)
       -- Even though the valId is escaping, this is safe because we know that our yielded value will go out before any potential FreeVal request could go out
       -- The FreeVal request will be done async after all sync frames.
